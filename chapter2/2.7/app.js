de40768c710c649e0450c0eeb0df5509a952c49d
konn3g <kikuya127@gmail.com>
2020-11-01 17:14:31 +0900

    追加: 2.7

---
+++ chapter2/2.7/app.js
@@ -0,0 +1,35 @@
+var items = [
+    {
+        name: '鉛筆',
+        price: 300,
+        quantity: 0
+    },
+    {
+        name: 'ノート',
+        price: 400,
+        quantity: 0
+    },
+    {
+        name: '消しゴム',
+        price: 500,
+        quantity: 0
+    },        
+]
+
+var vm = new Vue({
+    el: '#app',
+    data: {//dataプロパティ
+        items: items
+    },
+    filters: {
+        numberWithDelimiter: function(value){
+            if (!value) {
+                return '0'
+            }
+            return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+        },
+        enFilter: (value) => {
+            return `${value}円`;
+        }
+    }
+})
